1de8330a2777077e2c8046e86546c3d5aacee0ab
Enrico van Goor <enrico@enrico.vangoor.interwijs.nl>
2021-12-21 14:20:46 +0100

    added IPv6 support

--- block_ip.sh
+++ block_ip.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 #VERSION=1.0
 
+# Set this value to 1 if you want notifications about auto blocked ip's
+NOFIY_BY_EMAIL=1
+
+# Where you want the email to be sent to
+EMAIL=your@email.tld
+
+# Give your server a name for easy idenfication 
+SERVER=`hostname -s`
+
+if [ "${NOFIY_BY_EMAIL}" -gt 0 ]; then
+  echo "IP $value has been blocked for making $count failed login attempts
+  $data
+  `dig -x $value`" | mail -s "$SERVER:  blocked $value for $count failed attempts" $EMAIL
+fi
+
+SCRIPT=/usr/local/directadmin/scripts/custom/block_ip.sh
+ip=$value $SCRIPT
+exit $?;
+root@premium-hosting-01:/usr/local/directadmin/scripts/custom# cat block_ip.sh 
+#!/bin/sh
+#VERSION=1.0
+
 BF=/root/blocked_ips.txt
 EF=/root/exempt_ips.txt
 
@@ -10,13 +32,29 @@ BLOCK_CHAIN=dynamic
 curriptables()
 {
 	echo "<br><br><textarea cols=160 rows=60>";
-	shorewall show dynamic
+	/usr/sbin/shorewall show dynamic
+	/usr/sbin/shorewall6 show dynamic
 	echo "</textarea>";
 }
 
 if [ "$ip" = "" ]; then
         echo "No ip has been passed via env.";
         exit 1;
+fi
+
+VALID4=`ip -4 route get $ip > /dev/null 2>&1`
+RET4=$?
+VALID6=`ip -6 route get $ip > /dev/null 2>&1`
+RET6=$?
+
+if [ $RET4 == "0" ]
+then
+	IPVERSION="ipv4"
+elif [ $RET6 == "0" ]
+then
+	IPVERSION="ipv6"
+else
+	IPVERSION="0"
 fi
 
 ### Do we have a block file?
@@ -50,8 +88,21 @@ fi
 echo "Blocking $ip ...<br>";
 echo "$ip=dateblocked=`date +%s`" >> $BF;
 echo "Adding $ip into ${BLOCK_CHAIN} chain...";
-shorewall drop $ip
+
+if [ $IPVERSION == "ipv6" ]
+then
+	/usr/sbin/shorewall6 drop $ip
+elif [ $IPVERSION == "ipv4" ]
+then
+	/usr/sbin/shorewall drop $ip
+else
+	echo "$ip is not valid. Not blocking.";
+	curriptables
+	exit 2;
+fi
+
 echo "<br><br>Result:";
 curriptables
 
 exit 0;
+
--- unblock_ip.sh
+++ unblock_ip.sh
@@ -12,6 +12,21 @@ if [ "$ip" = "" ]; then
         exit 1;
 fi
 
+VALID4=`ip -4 route get $ip > /dev/null 2>&1`
+RET4=$?
+VALID6=`ip -6 route get $ip > /dev/null 2>&1`
+RET6=$?
+
+if [ $RET4 == "0" ]
+then
+	IPVERSION="ipv4"
+elif [ $RET6 == "0" ]
+then
+	IPVERSION="ipv6"
+else
+	IPVERSION="0"
+fi
+
 if [ ! -e "$BF" ]; then
         echo "cannot find $BF to unblock the IP";
         exit 2;
@@ -30,5 +45,18 @@ echo "Unblocking $ip ...";
 cat $BF | grep -v "^$ip=" > $BF.temp
 mv $BF.temp $BF
 chmod 600 $BF
-shorewall allow $ip
+
+if [ $IPVERSION == "ipv6" ]
+then
+	/usr/sbin/shorewall6 allow $ip
+elif [ $IPVERSION == "ipv4" ]
+then
+	/usr/sbin/shorewall allow $ip
+else
+	echo "$ip is not valid. Not blocking.";
+	curriptables
+	exit 2;
+fi
+
 exit 0;
+

736b0b5fa3d667bfffd9a4b34ea191896cbb609b
Interwijs <88405163+interwijs@users.noreply.github.com>
2022-01-05 11:27:18 +0100

    Update block_ip.sh

--- block_ip.sh
+++ block_ip.sh
@@ -1,28 +1,6 @@
 #!/bin/sh
 #VERSION=1.0
 
-# Set this value to 1 if you want notifications about auto blocked ip's
-NOFIY_BY_EMAIL=1
-
-# Where you want the email to be sent to
-EMAIL=your@email.tld
-
-# Give your server a name for easy idenfication 
-SERVER=`hostname -s`
-
-if [ "${NOFIY_BY_EMAIL}" -gt 0 ]; then
-  echo "IP $value has been blocked for making $count failed login attempts
-  $data
-  `dig -x $value`" | mail -s "$SERVER:  blocked $value for $count failed attempts" $EMAIL
-fi
-
-SCRIPT=/usr/local/directadmin/scripts/custom/block_ip.sh
-ip=$value $SCRIPT
-exit $?;
-root@premium-hosting-01:/usr/local/directadmin/scripts/custom# cat block_ip.sh 
-#!/bin/sh
-#VERSION=1.0
-
 BF=/root/blocked_ips.txt
 EF=/root/exempt_ips.txt
 
